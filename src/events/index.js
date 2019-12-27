ee1d0db0a0a1ea224974b1a495fa2a1f24b6001c
Mihai Babet <mihai.babet@gmail.com>
2019-12-27 22:51:20 +0200

    add eslint

--- src/events/index.js
+++ src/events/index.js
@@ -5,7 +5,7 @@ const _ = require('lodash')
 const handlers = []
 const resolvers = {}
 
-emitter.on( 'event', (action) => {
+emitter.on('event', (action) => {
   _.get(resolvers, action.type, []).forEach((resolve) => resolve(action))
   _.unset(resolvers, action.type)
   handlers.forEach((handler) => handler(action))
@@ -13,7 +13,7 @@ emitter.on( 'event', (action) => {
 
 module.exports = {
   subscribe: (handler) => handlers.push(handler),
-  dispatch: (action) => emitter.emit( 'event', action),
+  dispatch: (action) => emitter.emit('event', action),
   take: (type) => new Promise((resolve) => {
     _.update(resolvers, type, (list = []) => [...list, resolve])
   })
